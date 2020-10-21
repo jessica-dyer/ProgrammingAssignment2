64f5685c09c0891d9e2870a38d9f07bd3c1023c1
jessica-dyer <jessica.dyer@gmail.com>
2020-10-21 06:28:46 -0700

    Final cacematrix code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## And computes the inverse of the special "matrix" 
 
 makeCacheMatrix <- function(x = matrix()) {
   cachedInverse <- NULL
@@ -20,54 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get, getInverse = getInverse)
 }
 
-## Write a short comment describing this function
+## If the inverse has already been calculated, 
+## then the cachesolve should retrieve the inverse from the cache.
+## If the matrix has been changed, it will re-solve and cache the new answer 
 
 cacheSolve <- function(x, ...) {
         x$getInverse(...)
 }
-
-# makeVector <- function(x = numeric()) {
-#   m <- NULL
-#   set <- function(y) {
-#     x <<- y
-#     m <<- NULL
-#   }
-#   get <- function() x
-#   setmean <- function(mean) m <<- mean
-#   getmean <- function() m
-#   list(set = set, get = get,
-#        setmean = setmean,
-#        getmean = getmean)
-# }
-# 
-# cachemean <- function(x, ...) {
-#   m <- x$getmean()
-#   if(!is.null(m)) {
-#     message("getting cached data")
-#     return(m)
-#   }
-#   data <- x$get()
-#   m <- mean(data, ...)
-#   x$setmean(m)
-#   m
-# }
-# 
-# makeVectorBetterVersion <- function(x = numeric()) {
-#   mean <- NULL
-#   set <- function(y) {
-#     x <<- y
-#     mean <<- NULL
-#   }
-#   get <- function() x
-#   #setmean <- function(mean) mean <<- mean   # setMean is bad, get rid of it
-#   getMean <- function(...) {
-#     if(is.null(mean)) {
-#       message("doing the heavy calculations!")
-#       mean <<- mean(x, ...)
-#     }
-#     mean
-#   }
-#   list(set = set, get = get,
-#        getMean = getMean)
-# }
-
